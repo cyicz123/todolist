5453c14d4b7b7eebc1b963e3f9a374fa29d43e12
cyicz123 <cyicz123@outlook.com>
2023-03-07 23:48:14 +0800

    Feat: Add user CRUD functions to handler package
    This commit adds functions for adding, deleting, modifying, and querying users in the handler package. It also implements custom error codes and corresponding unit tests to ensure proper functionality.
    
    Signed-off-by: cyicz123 <cyicz123@outlook.com>

---
+++ user/internal/handler/user.go
@@ -0,0 +1,101 @@
+package handler
+
+import (
+	"regexp"
+
+	"github.com/cyicz123/todolist/user/pkg/e"
+	"github.com/cyicz123/todolist/user/pkg/logger"
+	"github.com/cyicz123/todolist/user/pkg/repo"
+)
+
+// UserInterface is an interface for user management operations.
+type UserInterface interface {
+	// Register creates a new user.
+	Register(info *repo.User) error
+
+	// Login returns the user after querying user info and checking the authentication.
+	Login(info *repo.User) error
+
+	// UserDelete deletes the user with the specified name.
+	UserDelete(name string) error
+}
+
+// UserInfo provides methods for user management operations.
+type UserInfo struct {
+	r repo.UserRepository
+	l logger.Interface
+}
+
+// Register creates a new user. Returns an error if the provided user name or password is invalid or if the user already exists.
+func (u *UserInfo) Register(info *repo.User) error {
+	if !u.checkUserName(info.UserName) {
+		u.l.Error(e.UserNameInvalid.Error())
+		return e.UserNameInvalid
+	}
+	if u.checkUserExist(info.UserName) {
+		u.l.Error(e.UserExist.Error())
+		return e.UserExist
+	}
+	if !u.checkPassword(info.PasswordDigest) {
+		u.l.Error(e.PasswordInvalid.Error())
+		return e.PasswordInvalid
+	}
+	err := u.r.Create(info)
+	if err != nil {
+		u.l.Error(e.UserInternalErr.Error())
+		return e.UserInternalErr
+	}
+	return nil
+}
+
+// Login returns the user with the specified name. Returns an error if the provided user name is invalid or if the user does not exist.
+func (u *UserInfo) Login(info *repo.User) error {
+	if !u.checkUserName(info.UserName) {
+		u.l.Error(e.UserNameInvalid.Error())
+		return e.UserNameInvalid
+	}
+	user, err := u.r.GetByName(info.UserName)
+	if err != nil {
+		u.l.Error(err)
+		return e.UserInternalErr
+	}
+	if user.PasswordDigest != info.PasswordDigest {
+		u.l.Warn(e.UserPasswdNotMatch.Error())
+		return e.UserPasswdNotMatch
+	}
+	*info = *user
+	return nil
+}
+
+// UserDelete deletes the user with the specified name. Returns an error if the provided user name is invalid or if the user does not exist.
+func (u *UserInfo) UserDelete(name string) error {
+	if !u.checkUserName(name) {
+		u.l.Error(e.UserNameInvalid.Error())
+		return e.UserNameInvalid
+	}
+	err := u.r.Delete(name)
+	if err != nil {
+		u.l.Error(e.UserInternalErr.Error())
+		return e.UserInternalErr
+	}
+	return nil
+}
+
+// checkUserName checks whether the specified user name is valid.
+func (u *UserInfo) checkUserName(name string) bool {
+	re := regexp.MustCompile(`^[a-zA-Z0-9_]{4,32}$`)
+    return re.MatchString(name)
+}
+
+// checkUserExist checks whether a user with the specified name already exists.
+func (u *UserInfo) checkUserExist(name string) bool {
+	if _, err := u.r.GetByName(name); err != nil {
+		return false
+	}
+	return true
+}
+
+// checkPassword checks whether the specified password is valid.
+func (u *UserInfo) checkPassword(pw string) bool {
+	return pw != ""
+}
---
+++ user/internal/handler/user_test.go
@@ -0,0 +1,385 @@
+package handler
+
+import (
+	"testing"
+
+	"github.com/cyicz123/todolist/user/mocks"
+	"github.com/cyicz123/todolist/user/pkg/e"
+	"github.com/cyicz123/todolist/user/pkg/repo"
+	"github.com/golang/mock/gomock"
+)
+
+type TestCases struct {
+	name     string
+	input    string
+	expected interface{}
+	mockExpected []interface{}
+}
+
+// var user repo.User
+
+var userNameTc = []TestCases{
+	{
+		name:     "Valid username with lowercase letters",
+		input:    "john_doe",
+		expected: true,
+	},
+	{
+		name:     "Valid username with uppercase letters",
+		input:    "John_Doe",
+		expected: true,
+	},
+	{
+		name:     "Valid username with digits",
+		input:    "johndoe123",
+		expected: true,
+	},
+	{
+		name:     "Invalid username with special character",
+		input:    "john@doe",
+		expected: false,
+	},
+	{
+		name:     "Invalid username that is too short",
+		input:    "j",
+		expected: false,
+	},
+	{
+		name:     "Invalid username that is too long",
+		input:    "this_username_is_way_too_long_to_be_valid",
+		expected: false,
+	},
+}
+
+func TestCheckUserName(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	log := mocks.NewMockInterface(ctrl)
+	r := mocks.NewMockUserRepository(ctrl)
+
+	u := &UserInfo{
+		l: log,
+		r: r,
+	}
+
+	for _, tc := range userNameTc {
+		t.Run(tc.name, func(t *testing.T) {
+			result := u.checkUserName(tc.input)
+			if result != tc.expected {
+				t.Errorf("expected %v, but got %v", tc.expected, result)
+			}
+		})
+	}
+}
+
+var passwordTc = []TestCases{
+	{
+		name:     "Invalid password that is null",
+		input:    "",
+		expected: false,
+	},
+	{
+		name:     "valid password",
+		input:    "Abc12345",
+		expected: true,
+	},
+}
+
+func TestCheckPassword(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	log := mocks.NewMockInterface(ctrl)
+	r := mocks.NewMockUserRepository(ctrl)
+
+	u := &UserInfo{
+		l: log,
+		r: r,
+	}
+
+	for _, tc := range passwordTc {
+		t.Run(tc.name, func(t *testing.T) {
+			result := u.checkPassword(tc.input)
+			if result != tc.expected {
+				t.Errorf("expected %v, but got %v", tc.expected, result)
+			}
+		})
+	}
+}
+
+var userExistTc = []TestCases{
+	{
+		name:     "Exist",
+		input:    "username1",
+		expected: true,
+		mockExpected: []interface{}{nil, nil},
+	},
+	{
+		name:     "Not exist",
+		input:    "username2",
+		expected: false,
+		mockExpected: []interface{}{nil, e.UserNotExist},
+	},
+}
+
+func TestCheckUserExist(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	log := mocks.NewMockInterface(ctrl)
+	r := mocks.NewMockUserRepository(ctrl)
+
+	u := &UserInfo{
+		l: log,
+		r: r,
+	}
+
+	for _, tc := range userExistTc {
+		t.Run(tc.name, func(t *testing.T) {
+			r.EXPECT().GetByName(tc.input).Return(tc.mockExpected...).Times(1)
+			result := u.checkUserExist(tc.input)
+			if result != tc.expected {
+				t.Errorf("expected %v, but got %v", tc.expected, result)
+			}
+		})
+	}
+}
+
+var registerTc = []struct{
+	name		string
+	input   	*repo.User
+	expected	error
+	mockExpected	[][]interface{}
+}{
+	{
+		name: "Valid user",
+		input: &repo.User{
+			UserName:        "valid_user",
+			PasswordDigest:  "Abc12345",
+		},
+		expected: nil,
+		mockExpected: [][]interface{}{
+			{
+				nil,
+				e.UserNotExist,
+			},
+			{nil},
+		},
+	},
+	{
+		name: "Invalid username",
+		input: &repo.User{
+			UserName:        "john@doe",
+			PasswordDigest:  "Abc12345",
+		},
+		expected: e.UserNameInvalid,
+		mockExpected: [][]interface{}{
+			{
+				nil, nil,
+			},
+			{nil},
+		},
+	},
+	{
+		name: "Existing user",
+		input: &repo.User{
+			UserName:        "existing_user",
+			PasswordDigest:  "Abc12345",
+		},
+		expected: e.UserExist,
+		mockExpected: [][]interface{}{
+			{
+				nil, nil,
+			},
+			{nil},
+		},
+	},
+	{
+		name: "Invalid password",
+		input: &repo.User{
+			UserName:        "valid_user",
+			PasswordDigest:  "",
+		},
+		expected: e.PasswordInvalid,
+		mockExpected: [][]interface{}{
+			{
+				nil, 
+				e.UserNotExist,
+			},
+			{nil},
+		},
+	},
+	{
+		name: "Create user failed",
+		input: &repo.User{
+			UserName:        "valid_user",
+			PasswordDigest:  "abc12345",
+		},
+		expected: e.UserInternalErr,
+		mockExpected: [][]interface{}{
+			{
+				nil,
+				e.UserNotExist,
+			},
+			{e.UserInternalErr},
+		},
+	},
+}
+
+func TestRegister(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	log := mocks.NewMockInterface(ctrl)
+	r := mocks.NewMockUserRepository(ctrl)
+
+	u := &UserInfo{
+		l: log,
+		r: r,
+	}
+	log.EXPECT().Error(gomock.Any()).AnyTimes()
+	for _, tc := range registerTc {
+		t.Run(tc.name, func(t *testing.T) {
+			r.EXPECT().GetByName(tc.input.UserName).Return(tc.mockExpected[0]...).MaxTimes(1)
+			r.EXPECT().Create(tc.input).Return(tc.mockExpected[1]...).MaxTimes(1)
+
+			result := u.Register(tc.input)
+			if result != tc.expected {
+				t.Errorf("expected %v, but got %v", tc.expected, result)
+			}
+		})
+	}
+}
+
+var loginTc = []struct{
+	name		string
+	input		*repo.User
+	expected	error
+	mockExpected	[]interface{}
+}{
+	{
+		name: "Valid user",
+		input: &repo.User{
+			UserName:        "valid_user",
+			PasswordDigest:  "Abc12345",
+		},
+		expected: nil,
+		mockExpected: []interface{}{
+			&repo.User{
+				UserID: 0,
+				UserName: "valid_user",
+				PasswordDigest: "Abc12345",
+				NickName: "valid_user",
+			},
+			nil,
+		},
+	},
+	{
+		name: "Invalid username",
+		input: &repo.User{
+			UserName:        "john@doe",
+			PasswordDigest:  "Abc12345",
+		},
+		expected: e.UserNameInvalid,
+		mockExpected: []interface{}{
+			nil, nil,
+		},
+	},
+	{
+		name: "Get user info failed",
+		input: &repo.User{
+			UserName:        "existing_user",
+			PasswordDigest:  "Abc12345",
+		},
+		expected: e.UserInternalErr,
+		mockExpected: []interface{}{
+			nil, e.UserInternalErr,
+		},
+	},
+	{
+		name: "User and password not match",
+		input: &repo.User{
+			UserName:        "valid_user",
+			PasswordDigest:  "abc12345",
+		},
+		expected: e.UserPasswdNotMatch,
+		mockExpected: []interface{}{
+			&repo.User{
+				UserName:	"valid_user",
+				PasswordDigest: "Abc12345",
+			},
+			nil,
+		},
+	},
+}
+
+func TestLogin(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	log := mocks.NewMockInterface(ctrl)
+	r := mocks.NewMockUserRepository(ctrl)
+
+	u := &UserInfo{
+		l: log,
+		r: r,
+	}
+	log.EXPECT().Error(gomock.Any()).AnyTimes()
+	log.EXPECT().Warn(gomock.Any()).AnyTimes()
+	for _, tc := range loginTc {
+		t.Run(tc.name, func(t *testing.T) {
+			r.EXPECT().GetByName(tc.input.UserName).Return(tc.mockExpected...).MaxTimes(1)
+
+			result := u.Login(tc.input)
+			if result != tc.expected {
+				t.Errorf("expected %v, but got %v", tc.expected, result)
+			}
+		})
+	}
+}
+
+var userDeleteTc = []TestCases{
+	{
+		name:     "success",
+		input:    "username",
+		expected: nil,
+		mockExpected: []interface{}{nil},
+	},
+	{
+		name:     "invalid username",
+		input:    "j",
+		expected: e.UserNameInvalid,
+		mockExpected: []interface{}{nil},
+	},
+	{
+		name:     "delete error",
+		input:    "username",
+		expected: e.UserInternalErr,
+		mockExpected: []interface{}{e.UserInternalErr},
+	},
+}
+
+func TestUserDelet(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	log := mocks.NewMockInterface(ctrl)
+	r := mocks.NewMockUserRepository(ctrl)
+
+	u := &UserInfo{
+		l: log,
+		r: r,
+	}
+	log.EXPECT().Error(gomock.Any()).AnyTimes()
+	for _, tc := range userDeleteTc {
+		t.Run(tc.name, func(t *testing.T) {
+			r.EXPECT().Delete(tc.input).Return(tc.mockExpected...).MaxTimes(1)
+
+			result := u.UserDelete(tc.input)
+			if result != tc.expected {
+				t.Errorf("expected %v, but got %v", tc.expected, result)
+			}
+		})
+	}
+}
